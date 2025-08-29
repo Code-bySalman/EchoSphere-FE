4ceb7f03fe1f6b9c23bedbfd08aeab7707df82a7
Code-bySalman <usmanis@student.iul.ac.in>
2025-08-29 16:25:28 +0530

    Rebuild dist with signup route

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const HOST = import.meta.env.VITE_SERVER_URL;
 
-export const AUTH_ROUTES = "api/auth";
-export const CONTACT_ROUTE = "api/contacts";
+export const AUTH_ROUTES = "/api/auth";
+export const CONTACT_ROUTE = "/api/contacts";
 
 
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
